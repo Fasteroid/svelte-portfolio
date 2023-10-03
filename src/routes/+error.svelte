f8bdd04d2749f8f0b7463d8af1ca3bfe207eeb1a
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2023-10-03 02:07:04 -0400

    lol

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { base } from "$app/paths";
+</script>
+
+<svelte:head>
+    <title>OH NAR</title> 
+    <style lang="scss">
+        @import "src/styles/goofy404.scss";
+    </style>
+</svelte:head>
+
+<body>
+    <div class="margins">
+        <section>
+            <h1>
+                <b>404</b>
+            </h1>
+            <div class="error-centerer">
+                <img src="{base}/assets/404.gif" alt="bikini atoll explosion">
+            </div>
+        </section>
+    </div>
+</body>
